b547ef79862903f8ce4a879aaf16c968236e226b
Diogo de Lima Gama da Silva <diligasi@gmail.com>
2013-04-20 03:50:34 -0300

    Base project
    Basic Ruby on Rails app
    - Devise installed;
    - CSS and Javascript added;
    - Basic structure for layout implemented;
    - Home and About pages basic structure created.

---
+++ README.md
@@ -0,0 +1,9 @@
+Monitoring system and blog oriented for research.
+
+- System informations
+
+	- Used Language:	Ruby,	version 1.9.3p125
+	- Used Framework:	Rails,	version 3.2.12
+	- Database:			MySQL
+
+By: Diogo de Lima Gama da Silva
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BugBrotherBrazil::Application.config.secret_token = 'f22d8f184f809a3698dfdc3ed3633eec841f5aaa946038aceddc80b6e6aa904c807e83a96eacf342fee011e17f546d9200ca565d3d3fb878bfa4ed4ccf8a836b'
